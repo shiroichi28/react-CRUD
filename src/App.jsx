7f7827d71831bd3f531a6d6567efb9207f71c0c7
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-27 22:08:39 +0530

    Added Create Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { ListUser } from "./components/userList";
+import { AddUser } from "./components/addUser";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/user" element={<ListUser />} />
+          <Route path="/create-user" element={<AddUser />} />
         </Routes>
       </div>
     </>
