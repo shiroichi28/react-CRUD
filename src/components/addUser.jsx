39400c37836a2a27cf6a2e4d1277d9780876b11f
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-27 22:09:01 +0530

    Intial Commit for Create

---
+++ src/components/addUser.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export const AddUser = () => {
+  const redirect = useNavigate();
+  const [response, setResponse] = useState("");
+  const [formValue, setFormValue] = useState({
+    username: "",
+    email: "",
+    mobile: "",
+  });
+
+  // Function to handle input changes
+  const handleInput = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      username: formValue.username,
+      email: formValue.email,
+      mobile: formValue.mobile,
+    };
+    try {
+      const res = await axios.post(
+        "http://localhost/react-crud-php/api/user.php",
+        formData
+      );
+      if (res.data.success) {
+        setResponse(res.data.success);
+        setTimeout(() => {
+          redirect("/user");
+        }, 2000);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center align-content-center vh-100">
+          <div className="col-md-4">
+            {/* Display response message */}
+            <div
+              className={
+                response && "alert alert-info alert-dismissible fade show"
+              }
+              role="alert"
+            >
+              {response}
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title text-center p-3">Add</h4>
+                {/* Form */}
+                <form method="post" autoComplete="off" onSubmit={handleSubmit}>
+                  <div className="mb-3 input-group">
+                    <span className="input-group-text">
+                      <i className="fa-solid fa-user" />
+                    </span>
+                    {/* Username input */}
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      className="form-control"
+                      placeholder="Username"
+                      value={formValue.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div className="mb-3 input-group">
+                    <span className="input-group-text">
+                      <i className="fa-solid fa-envelope" />
+                    </span>
+                    {/* Email input */}
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      className="form-control"
+                      placeholder="Email"
+                      pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                      value={formValue.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div className="mb-3 input-group">
+                    <span className="input-group-text">
+                      <i className="fa-solid fa-mobile-retro" />
+                    </span>
+                    {/* Mobile input */}
+                    <input
+                      type="tel"
+                      name="mobile"
+                      id="mobile"
+                      className="form-control"
+                      placeholder="Mobile"
+                      pattern="^[6-9]\d{9}$"
+                      maxLength={10}
+                      value={formValue.mobile}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    {/* Submit button */}
+                    <button
+                      type="submit"
+                      name="submit"
+                      className="btn btn-success w-40"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
