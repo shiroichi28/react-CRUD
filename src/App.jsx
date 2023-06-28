971056738fc9bca22a706e72add8e87c25c3632a
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-28 23:07:53 +0530

    Route for Edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./components/Home";
 import { ListUser } from "./components/userList";
 import { AddUser } from "./components/addUser";
 import { Routes, Route } from "react-router-dom";
+import { EditUser } from "./components/editUser";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/user" element={<ListUser />} />
           <Route path="/create-user" element={<AddUser />} />
+          <Route path="/edit-user/:id" element={<EditUser />} />
         </Routes>
       </div>
     </>
