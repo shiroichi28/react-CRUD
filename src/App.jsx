d81604881584fd51b872bc032c243106dab88f19
shiroichi28 <manikandanmk2972@gmail.com>
2023-07-02 23:00:30 +0530

    Implement LazyLoad

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
+import React, { lazy, Suspense } from "react";
 import { Header } from "./components/Header";
-import { Home } from "./components/Home";
-import { ListUser } from "./components/userList";
-import { AddUser } from "./components/addUser";
 import { Routes, Route } from "react-router-dom";
-import { EditUser } from "./components/editUser";
+
+const Home = lazy(() => import("./components/Home"));
+const ListUser = lazy(() => import("./components/userList"));
+const AddUser = lazy(() => import("./components/addUser"));
+const EditUser = lazy(() => import("./components/editUser"));
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user" element={<ListUser />} />
-          <Route path="/create-user" element={<AddUser />} />
-          <Route path="/edit-user/:id" element={<EditUser />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="loader-container">
+              <span className="loader"></span>
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<ListUser />} />
+            <Route path="/create-user" element={<AddUser />} />
+            <Route path="/edit-user/:id" element={<EditUser />} />
+          </Routes>
+        </Suspense>
       </div>
     </>
   );
